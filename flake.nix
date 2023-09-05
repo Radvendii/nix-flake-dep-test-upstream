0b374b4cd635fc92bd83206913d01e41b42e141d
Taeer Bar-Yam <taeer.bar-yam@tweag.io>
2023-09-05 12:18:24 -0400

    foo

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       ) > $out
     '';
 
-    packges.x86_64-linux.foo = builtins.trace "${self}" (import "${self}/foo.nix" nixpkgs.legacyPackages.x86_64-linux);
+    packages.x86_64-linux.foo = builtins.trace "${self}" (import "${self}/foo.nix" nixpkgs.legacyPackages.x86_64-linux);
 
   };
 }
