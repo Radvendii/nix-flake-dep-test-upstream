357b86dab5cd347350866a064b4b57ad0c91722e
Taeer Bar-Yam <taeer.bar-yam@tweag.io>
2023-09-11 10:19:33 -0400

    A

--- flake.nix
+++ flake.nix
@@ -1,21 +1,12 @@
 {
   description = "A very basic flake";
 
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+
   outputs = { self, nixpkgs }: {
 
     packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.runCommand "name" {} ''
-      (
-        echo "self: ${self}"
-        echo "rev: ${self.rev or "none"}"
-        echo "shortRev: ${self.shortRev or "none"}"
-        echo "lastModified: ${builtins.toString self.lastModified}"
-        echo "lastModifiedDate: ${self.lastModifiedDate}"
-        echo "narHash: ${self.narHash}"
-        echo "outPath: ${self.outPath}"
-      ) > $out
+      echo ${nixpkgs.lib.version} > $out
     '';
-
-    packages.x86_64-linux.foo = builtins.trace "${self}" (import "${self}/foo.nix" nixpkgs.legacyPackages.x86_64-linux);
-
   };
 }
