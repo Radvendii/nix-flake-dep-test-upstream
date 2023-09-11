f880688a4dd4345a1f2320ba4cd47934b03ad816
Taeer Bar-Yam <taeer.bar-yam@tweag.io>
2023-09-11 10:20:09 -0400

    B

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A very basic flake";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: {
 
