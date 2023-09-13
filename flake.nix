c5ef581991f9596ebe13facf58b546cf7745f594
Taeer Bar-Yam <taeer.bar-yam@tweag.io>
2023-09-13 10:32:33 -0400

    upstream has to have a stable uri to trigger bug

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
   description = "A very basic flake";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
   outputs = { self, nixpkgs }: {
 
     packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.runCommand "name" {} ''
-      echo ${nixpkgs.lib.version} > $out
+      echo ${nixpkgs.shortRev} > $out
     '';
   };
 }
